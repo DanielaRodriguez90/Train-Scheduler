16151ac89b2e460de2cbc34674e5efd28bc90a5a
Dani_Cami <danielarodriguez@Danielas-MacBook-Pro.local>
2019-04-22 17:54:50 -0400

    button/submit/click grabbing form values

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,3 +12,42 @@ var config = {
 
   //set database as a variable
   var database = firebase.database();
+
+  //clic listener for button adding new train
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+
+    //grab train inputs on form
+    var trainName = $("#train-name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var firstTrain = moment($("#first-train-input").val().trim(),"H:M").format("H:M");
+    var frequency = $("#frequency-input").val().trim();
+
+    ///create new train obj
+
+    var newTrain ={
+        name: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
+    };
+
+    ///push newTrain obj to firebase
+    database.ref().push(newTrain);
+
+    //console.log
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTrain);
+    console.log(newTrain.frequency);
+
+    alert("New Train Added, With Destination To:" + "_" + newTrain.destination);
+
+    // clear all inputs
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-train-input").val("");
+    $("#frequency-input").val("");
+  });
+
+  
